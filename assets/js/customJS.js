a12bc42d5441de42b01d71fc2c8739a7e7ff9dd1
Dinesh <68639217+dineshann@users.noreply.github.com>
2022-09-21 09:04:57 +0530

    rel

--- assets/js/customJS.js
+++ assets/js/customJS.js
@@ -5,6 +5,185 @@ $(document).ready(function(){
     // ASO.init();    
 
     // Slick-Slide
+    $(".stateViewSlick").slick({
+        slidesToShow: 5,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        slidesToScroll: 4,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+
+    $(".otherSrvSlick").slick({
+        slidesToShow: 5,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+
+    $(".popularSrvSlick").slick({
+        slidesToShow: 4,
+        infinite: true,
+        speed: 500,
+        centerPadding: '20px',
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+
+    $(".seasonsSlick").slick({
+        slidesToShow: 4,
+        infinite: true,
+        speed: 500,
+        centerPadding: '20px',
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+
+    $(".geiTastesSlick").slick({
+        slidesToShow: 4,
+        infinite: true,
+        speed: 500,
+        centerPadding: '20px',
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+
+    $(".dateViewSlick").slick({
+        slidesToShow: 6,
+        speed: 500,
+        infinite: false,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        slidesToScroll: 4,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 3
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '20px',
+                slidesToShow: 1
+            }
+            }
+        ]
+    });
+    
     $(".custom-slide").slick({
         slidesToShow: 4,
         infinite: false,
